f91e6722c41b17ad3e814a2225269006d407ff0f
0x-devSanjana <sanjana.prakash110@gmail.com>
2026-09-07 22:19:37 +0530

    Add files via upload

---
+++ static/sw.js
@@ -0,0 +1,36 @@
+const CACHE = "studyos-v2";
+const APP_SHELL = [
+  "/",
+  "/static/style.css",
+  "/static/app.js",
+  "/static/manifest.webmanifest",
+  "/static/icons/icon-192.png",
+  "/static/icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  const req = event.request;
+  if (req.method !== "GET") return;
+
+  event.respondWith(
+    fetch(req).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE).then(cache => cache.put(req, copy));
+      return response;
+    }).catch(() => caches.match(req).then(r => r || caches.match("/")))
+  );
+});
